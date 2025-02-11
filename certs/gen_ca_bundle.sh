202286ccef0e53cd253e3510d6ad897d7f495710
Ted Mule <daddvted@gmail.com>
2025-02-11 11:26:12 +0800

    update mutant certs
    Signed-off-by: Ted Mule <daddvted@gmail.com>

--- certs/gen_ca_bundle.sh
+++ certs/gen_ca_bundle.sh
@@ -4,11 +4,12 @@ SAN=san.cnf
 SERVER_NAME=server
 ROOT_NAME=rootCA
 
+# certificate and key are used for mutant application
 # Generate server key and csr 
 openssl req -newkey rsa:2048 -nodes -sha256 -keyout ${SERVER_NAME}.key -out ${SERVER_NAME}.csr -config san.cnf
-
 # Sign server certificate using CA
 openssl x509 -req -in ${SERVER_NAME}.csr -CA ${ROOT_NAME}.crt -CAkey ${ROOT_NAME}.key -out ${SERVER_NAME}.crt -CAcreateserial -days 3650 -extfile $SAN -extensions v3_req
 
 # generate ca bundle
+# CA bundle is used for MutatingWebhookConfiguration
 cat rootCA.crt | base64 -w 0
